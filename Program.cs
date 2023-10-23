87249516f6e9f05de375f9741aa548bb031fe0ce
abelponce03 <abelponce03@gmail.com>
2023-10-23 17:28:35 -0400

    Estructura

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace Jose
+{
+
+    public enum TipoDCoordenadas { Geograficas, Cilindricas, Esfericas, Cartesianas }
+    public abstract class Punto
+    {
+        public TipoDCoordenadas Tipo { get;set; }
+    }
+    public class CooCartesiana : Punto
+    {
+        public CooCartesiana(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Tipo = TipoDCoordenadas.Cartesianas;
+        }
+        
+
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+    }
+    public class CooEsfericas : Punto
+    {
+        public CooEsfericas(double modulo,  double omega,double cita)
+        {
+           Modulo  = modulo;
+            Cita = cita;
+            Omega = omega;
+            Tipo = TipoDCoordenadas.Esfericas;
+        }
+        
+
+        public double Modulo { get; }
+        public double Cita { get; }
+        public double Omega { get; }
+    }
+    public class CooCilindrica : Punto
+    {
+        public CooCilindrica(double modulo,  double omega,double z)
+        {
+           Modulo  = modulo;
+            Z = z;
+            Omega = omega;
+            Tipo = TipoDCoordenadas.Cilindricas;
+        }
+        
+
+        public double Modulo { get; }
+        public double Z { get; }
+        public double Omega { get; }
+    }
+    public class CooGeograficas : Punto
+    {
+        public CooGeograficas(double coordenadasEO,  double coordenadasNS,char EoO,char noS)
+        {
+           CoordenadasNS  = coordenadasNS;
+            CoordenadasEO = coordenadasEO;
+            EsteoOeste = EoO;
+            NoS = noS;
+            Tipo = TipoDCoordenadas.Geograficas;
+        }
+        
+
+        public double CoordenadasNS { get; }
+        public double CoordenadasEO { get; }
+        public char EsteoOeste { get; }
+        public char NoS { get; }
+    }
+
+    public static class Convertir
+    {
+        public static 
+    }
+}
